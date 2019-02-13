fff7c260a3b71f87b6f456e095d50ccb226b5153
Sergey Vlasik <vlasmarkiz@gmail.com>
2019-02-13 23:23:09 +0200

    Задача 2ÑÐÐÑ

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+//Задание 1
 $name = 'Сергей';
 $age = '28';
 echo "Меня зовут: $name <br>";
@@ -6,4 +7,10 @@ echo "Мне $age лет <br>";
 $a = '"!|\/\'';
 $b = "\"\\";
 echo $a;
-echo $b;
+echo "$b <br>";
+//Задание 2
+define("FLOMASTER", 23);
+define("KARANDASH", 40);
+define("SUMM", 80);
+$kraski = SUMM - KARANDASH - FLOMASTER;
+echo SUMM . '-' . KARANDASH . '-' . FLOMASTER . '=' . $kraski;
