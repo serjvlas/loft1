d2555f46e5f480a420c65e85fcd5046bec208595
Sergey Vlasik <vlasmarkiz@gmail.com>
2019-02-17 13:21:17 +0200

    task4

--- task4.php
+++ task4.php
@@ -1,7 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 14.02.2019
- * Time: 21:55
- */
+$day = 4;
+switch ($day) {
+    case 1:
+        echo 'Это рабочий день';
+        break;
+    case 2:
+        echo 'Это рабочий день';
+        break;
+    case 3:
+        echo 'Это рабочий день';
+        break;
+    case 4:
+        echo 'Это рабочий день';
+        break;
+    case 5:
+        echo 'Это рабочий день';
+        break;
+    case 6:
+        echo 'Это выходной день';
+        break;
+    case 7:
+        echo 'Это выходной день';
+        break;
+    default:
+        echo 'Неизвестный день';
+}
