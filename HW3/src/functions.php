a34ab6c26fafec3eb7c6b36133abd80ca46dfa99
Sergey Vlasik <vlasmarkiz@gmail.com>
2019-02-21 23:22:09 +0200

    DZ3task1

--- HW3/src/functions.php
+++ HW3/src/functions.php
@@ -1,12 +1,9 @@
 <?php
 function task1 (){
-    $dataFile = file_get_contents ('data.xml');
+    $dataFile = file_get_contents('data.xml');
     $xml = new SimpleXMLElement($dataFile);
 
-   // echo '<pre>';
-  //  var_dump($xml);
-    ///var_dump($orderNumber);
-  foreach ($xml->Address as $address) {
+    foreach ($xml->Address as $address) {
         echo $address->attributes() . ':' . '<br>';
         echo $address->Name . '<br>';
         echo $address->Street . '<br>';
@@ -15,18 +12,18 @@ function task1 (){
         echo $address->Zip . '<br>';
         echo $address->Country . '<br>' . '<pre></pre>';
     }
-  echo '<br>';
-
-  foreach ($xml->Items->Item as $items) {
-      $atrib = $items[PartNumber] . '<br>';
-      echo 'Part Number: ' . $atrib;
-      echo 'Product name: ' . $items->ProductName . '<br>';
-      echo 'Quantity: ' . $items->Quantity . '<br>';
-      echo 'USPrice: ' . $items->USPrice . '<br>';
-      echo 'Comment: ' . $items->Comment . '<br>';
-      echo 'Ship Date: ' . $items->ShipDate . '<br>';
       echo '<br>';
-  }
+
+    foreach ($xml->Items->Item as $items) {
+        $atrib = $items[PartNumber] . '<br>';
+        echo 'Part Number: ' . $atrib;
+        echo 'Product name: ' . $items->ProductName . '<br>';
+        echo 'Quantity: ' . $items->Quantity . '<br>';
+        echo 'USPrice: ' . $items->USPrice . '<br>';
+        echo 'Comment: ' . $items->Comment . '<br>';
+        echo 'Ship Date: ' . $items->ShipDate . '<br>';
+        echo '<br>';
+    }
     $delNotes = $xml->DeliveryNotes;
     echo 'Delivery Notes: ' .  $delNotes . '<br>';
 }
