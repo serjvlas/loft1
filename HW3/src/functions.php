e7691e618a608d636e7a8e68c2bf77d7352ba14d
Sergey Vlasik <vlasmarkiz@gmail.com>
2019-02-23 00:23:31 +0200

    DZ3task2

--- HW3/src/functions.php
+++ HW3/src/functions.php
@@ -1,5 +1,6 @@
 <?php
-function task1 (){
+function task1()
+{
     $dataFile = file_get_contents('data.xml');
     $xml = new SimpleXMLElement($dataFile);
 
@@ -28,25 +29,27 @@ function task1 (){
     echo 'Delivery Notes: ' .  $delNotes . '<br>';
 }
 //task1 ();
-function task2 () {
+function task2()
+{
     $employers = ["Dima","Misha","Tanya"];
-
     $json = json_encode($employers);
     file_put_contents("output.json", $json);
-    if ((rand(1,2)) === 1) {
+    if ((rand(1, 2)) === 1) {
         $jsonDecode = json_decode($json);
         $replaceArr = ["Tanya","Masha", "Sveta"];
-        $newJson = array_replace ($jsonDecode, $replaceArr);
+        $newJson = array_replace($jsonDecode, $replaceArr);
         $newJson = json_encode($newJson);
-        file_put_contents("output2.json",$newJson);
+        file_put_contents("output2.json", $newJson);
     }
     $output = file_get_contents("output.json");
-    $jsonoutput = json_decode($output);
+    $jsonoutput = json_decode($output, true);
     $output2 = file_get_contents("output2.json");
-    $jsonoutput2 = json_decode($output2);
+    $jsonoutput2 = json_decode($output2, true);
     $arrDiffs = array_diff($jsonoutput, $jsonoutput2);
-    echo "<pre>";
-   var_dump($arrDiffs);
+    echo "Отличаются: " . "<br>";
+    foreach ($arrDiffs as $diff) {
+        echo $diff . "<br>";
+    }
 }
 //task2();
 
