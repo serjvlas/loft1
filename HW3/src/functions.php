5ed354e98c3ad67376e3401843594cb544ddd1bb
Sergey Vlasik <vlasmarkiz@gmail.com>
2019-02-23 00:06:42 +0200

    DZ3task2

--- HW3/src/functions.php
+++ HW3/src/functions.php
@@ -28,4 +28,28 @@ function task1 (){
     echo 'Delivery Notes: ' .  $delNotes . '<br>';
 }
 //task1 ();
+function task2 () {
+    $employers = ["Dima","Misha","Tanya"];
 
+    $json = json_encode($employers);
+    file_put_contents("output.json", $json);
+    if ((rand(1,2)) === 1) {
+        $jsonDecode = json_decode($json);
+        $replaceArr = ["Tanya","Masha", "Sveta"];
+        $newJson = array_replace ($jsonDecode, $replaceArr);
+        $newJson = json_encode($newJson);
+        file_put_contents("output2.json",$newJson);
+    }
+    $output = file_get_contents("output.json");
+    $jsonoutput = json_decode($output);
+    $output2 = file_get_contents("output2.json");
+    $jsonoutput2 = json_decode($output2);
+    $arrDiffs = array_diff($jsonoutput, $jsonoutput2);
+    echo "<pre>";
+   var_dump($arrDiffs);
+}
+//task2();
+
+
+
+//task2JSON ($employers);
